541f379adc65dba1bf1033d934c41d27b0741de4
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-26 15:28:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = "3845818"
 API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
 BOT_TOKEN = "5426835246:AAGpWNE1EQF5_t56BmcdD8QWSJhCtynh3gk"
 ADMIN = "1443454117"         
-CAPTION = "{filename}"
+CAPTION = "{file_name}"
 
 class temp(object):
     THUMBNAIL = ""
