fc864370384a5aa7b01590c2846a16c7e58babb7
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-26 15:16:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = "3845818"
+API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
+BOT_TOKEN = "5426835246:AAGpWNE1EQF5_t56BmcdD8QWSJhCtynh3gk"
+ADMIN = "1443454117"         
+CAPTION = "{filename}"
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = ""
