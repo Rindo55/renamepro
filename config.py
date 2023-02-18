1f1e0d3dd02394f6a2db21dc16b83d18f156e4a8
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-18 09:46:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = "3845818"
 API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
-BOT_TOKEN = "5426835246:AAGpWNE1EQF5_t56BmcdD8QWSJhCtynh3gk"
+BOT_TOKEN = "6284581524:AAEHd9Idbj2sk5bARTkXX1CS_dGOXOgXuy4"
 ADMIN = "1443454117"         
 CAPTION = "{file_name}"
 
