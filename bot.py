6f7782ba68c483192b6275ecc535510a625e6ffa
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-18 09:30:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from aiohttp import web
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            session_name="simple-renamer",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
