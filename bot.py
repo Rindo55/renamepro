9bccb50875d5ac6a383c1cb3cc59a6c87d4e5d50
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-13 11:02:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ from config import *
 from route import web_server
 from aiohttp import web
 bot = Client(
-            name="renamerx",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=100,
-            plugins={"root": "main"},
-            sleep_threshold=10,
-        )
+    "bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=100,
+    plugins={"root": "main"},
+    sleep_threshold=10,
+  )
 bot.start()
        
