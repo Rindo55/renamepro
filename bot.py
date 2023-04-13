35b5421c89c81dec95d4e9fa2800bf767bb65269
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-13 11:07:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 from pyrogram import Client
 from config import *
-import os
+from route import web_server
+from aiohttp import web
 
 class Bot(Client):
-    if not os.path.isdir(DOWNLOAD_LOCATION):
-        os.makedirs(DOWNLOAD_LOCATION)
-
     def __init__(self):
         super().__init__(
             name="simple-renamer",
@@ -18,7 +16,12 @@ class Bot(Client):
         )
     async def start(self):
         await super().start()
-        me = await self.get_me()      
+        me = await self.get_me()   
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        port = "8080"
+        await web.TCPSite(app, bind_address, port).start()   
         print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
        
     async def stop(self, *args):
